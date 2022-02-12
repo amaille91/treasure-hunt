ecc9ae78156fa6dd4644b0c2027d36dfa9d70fb2
Alexandre Maille <an email>
2022-02-12 15:40:34 +0100

    TDD - treasures lexing

---
+++ src/main/java/TreasureLineToken.java
@@ -0,0 +1,11 @@
+public class TreasureLineToken implements LineToken {
+
+	public Position getPosition() {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	public int getNumber() {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+}
--- src/test/java/LexerTest.java
+++ src/test/java/LexerTest.java
@@ -85,36 +85,94 @@ public class LexerTest {
 		@Test
 		void lines_not_constituted_of_less_than_3_components_separated_by_dashes_should_result_in_LineFormatException()
 				throws Throwable {
-			String mapLineStr = "M - 20";
-			assertException(LineFormatException.class, assertLineFormatExceptionWithLine(mapLineStr),
-					() -> Lexer.toLineToken(mapLineStr));
+			String mountainLineStr = "M - 20";
+			assertException(LineFormatException.class, assertLineFormatExceptionWithLine(mountainLineStr),
+					() -> Lexer.toLineToken(mountainLineStr));
 		}
 		
 		@Test
 		void lines_not_constituted_of_more_than_3_components_separated_by_dashes_should_result_in_LineFormatException()
 				throws Throwable {
-			String mapLineStr = "M - 20 - 15 - 12";
-			assertException(LineFormatException.class, assertLineFormatExceptionWithLine(mapLineStr),
-					() -> Lexer.toLineToken(mapLineStr));
+			String mountainLineStr = "M - 20 - 15 - 12";
+			assertException(LineFormatException.class, assertLineFormatExceptionWithLine(mountainLineStr),
+					() -> Lexer.toLineToken(mountainLineStr));
 		}
 		
 		@Test
-		void lines_not_starting_with_C_should_result_in_LineFormatException() throws Throwable {
-			String mapLineStr = "anythingOtherThanM - 20 - 15";
-			assertException(LineFormatException.class, assertLineFormatExceptionWithLine(mapLineStr),
-					() -> Lexer.toLineToken(mapLineStr));
+		void lines_not_starting_with_M_should_result_in_LineFormatException() throws Throwable {
+			String mountainLineStr = "anythingOtherThanM - 20 - 15";
+			assertException(LineFormatException.class, assertLineFormatExceptionWithLine(mountainLineStr),
+					() -> Lexer.toLineToken(mountainLineStr));
 		}
 		
 		@Test
-		void malformed_numbers_in_map_lines_should_result_in_NumberFormatException_first_number() throws Exception {
-			String mapLineStr = "C - 15toto - 20";
-			assertThrows(NumberFormatException.class, () -> Lexer.toLineToken(mapLineStr));
+		void malformed_numbers_in_mountain_lines_should_result_in_NumberFormatException_first_number() throws Exception {
+			String mountainLineStr = "M - 15toto - 20";
+			assertThrows(NumberFormatException.class, () -> Lexer.toLineToken(mountainLineStr));
 		}
 		
 		@Test
-		void malformed_numbers_in_map_lines_should_result_in_NumberFormatException_second_number() throws Exception {
-			String mapLineStr = "C - 15 - 2a0";
-			assertThrows(NumberFormatException.class, () -> Lexer.toLineToken(mapLineStr));
+		void malformed_numbers_in_mountain_lines_should_result_in_NumberFormatException_second_number() throws Exception {
+			String mountainLineStr = "M - 15 - 2a0";
+			assertThrows(NumberFormatException.class, () -> Lexer.toLineToken(mountainLineStr));
+		}
+	}
+	
+	@Nested
+	class ParsingTreasuresLines {
+		
+		@Test
+		void parsing_a_treasure_line_should_result_in_a_treasureLine_object() throws Exception {
+			String mapLineStr = "T  - 12 - 155   - 3";
+			
+			LineToken lineToken = Lexer.toLineToken(mapLineStr);
+			
+			assertThat(lineToken).isInstanceOfSatisfying(TreasureLineToken.class,
+					treasureLineToken -> {
+						assertThat(treasureLineToken.getPosition()).usingRecursiveComparison().isEqualTo(new Position(12, 155));
+						assertThat(treasureLineToken.getNumber()).isEqualTo(3);
+					});
+		}
+		
+		@Test
+		void lines_not_constituted_of_less_than_4_components_separated_by_dashes_should_result_in_LineFormatException()
+				throws Throwable {
+			String treasureLineStr = "T - 20 - 3";
+			assertException(LineFormatException.class, assertLineFormatExceptionWithLine(treasureLineStr),
+					() -> Lexer.toLineToken(treasureLineStr));
+		}
+		
+		@Test
+		void lines_not_constituted_of_more_than_4_components_separated_by_dashes_should_result_in_LineFormatException()
+				throws Throwable {
+			String treasureLineStr = "M - 20 - 15 - 12 - 1";
+			assertException(LineFormatException.class, assertLineFormatExceptionWithLine(treasureLineStr),
+					() -> Lexer.toLineToken(treasureLineStr));
+		}
+		
+		@Test
+		void lines_not_starting_with_T_should_result_in_LineFormatException() throws Throwable {
+			String treasureLineStr = "R - 20 - 15 - 1";
+			assertException(LineFormatException.class, assertLineFormatExceptionWithLine(treasureLineStr),
+					() -> Lexer.toLineToken(treasureLineStr));
+		}
+		
+		@Test
+		void malformed_numbers_in_treasure_lines_should_result_in_NumberFormatException_first_number() throws Exception {
+			String treasureLineStr = "T - 15toto - 20 - 1";
+			assertThrows(NumberFormatException.class, () -> Lexer.toLineToken(treasureLineStr));
+		}
+		
+		@Test
+		void malformed_numbers_in_treasure_lines_should_result_in_NumberFormatException_second_number() throws Exception {
+			String treasureLineStr = "T - 15 - 2a0 - 1";
+			assertThrows(NumberFormatException.class, () -> Lexer.toLineToken(treasureLineStr));
+		}
+		
+		@Test
+		void malformed_numbers_in_treasure_lines_should_result_in_NumberFormatException_third_number() throws Exception {
+			String treasureLineStr = "T - 15 - 20 - x1";
+			assertThrows(NumberFormatException.class, () -> Lexer.toLineToken(treasureLineStr));
 		}
 	}
 
