1ca2e067cd3c34b90fc5716957646303b5bdd6bd
Alexandre Maille <an email>
2022-02-13 11:13:23 +0100

    TDD - adding cases for map sizes

--- src/test/java/parsing/SimulationBuilderTest.java
+++ src/test/java/parsing/SimulationBuilderTest.java
@@ -2,6 +2,7 @@ package parsing;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import lexing.model.MapLineToken;
@@ -9,11 +10,38 @@ import parsing.exceptions.IllegalMapSizeException;
 
 public class SimulationBuilderTest {
 	
-	@Test
-	void initializing_builder_with_negative_sizes_should_result_in_IllegalMapSizeException_first_size() throws Exception {
-		MapLineToken mapLineToken = new MapLineToken(-1, 10);
+	@Nested
+	class StrictlyPositiveMapSizes {
+
+		@Test
+		void initializing_builder_with_negative_sizes_should_result_in_IllegalMapSizeException_first_size() throws Exception {
+			MapLineToken mapLineToken = new MapLineToken(-1, 10);
+			
+			assertThrows(IllegalMapSizeException.class, () -> new SimulationBuilder(mapLineToken));
+		}
 		
-		assertThrows(IllegalMapSizeException.class, () -> new SimulationBuilder(mapLineToken));
+		@Test
+		void initializing_builder_with_negative_sizes_should_result_in_IllegalMapSizeException_second_size() throws Exception {
+			MapLineToken mapLineToken = new MapLineToken(3, -1);
+			
+			assertThrows(IllegalMapSizeException.class, () -> new SimulationBuilder(mapLineToken));
+		}
+		
+		@Test
+		void initializing_builder_with_zero_size_should_result_in_IllegalMapSizeException_first_size() throws Exception {
+			MapLineToken mapLineToken = new MapLineToken(0, 10);
+			
+			assertThrows(IllegalMapSizeException.class, () -> new SimulationBuilder(mapLineToken));
+		}
+		
+		@Test
+		void initializing_builder_with_zero_sizes_should_result_in_IllegalMapSizeException_second_size() throws Exception {
+			MapLineToken mapLineToken = new MapLineToken(3, 0);
+			
+			assertThrows(IllegalMapSizeException.class, () -> new SimulationBuilder(mapLineToken));
+		}
+		
 	}
+	
 
 }
