de345ecd6d5e5e34e1ce2f4d0458fb5cf4bce9c6
Alexandre Maille <an email>
2022-02-13 11:24:53 +0100

    TDD - out of bounds terrain should result in OutboundTerrainException

---
+++ src/main/java/parsing/exceptions/OutboundTerrainException.java
@@ -0,0 +1,5 @@
+package parsing.exceptions;
+
+public class OutboundTerrainException extends RuntimeException {
+	private static final long serialVersionUID = 6193468914242879868L;
+}
--- src/test/java/parsing/SimulationBuilderTest.java
+++ src/test/java/parsing/SimulationBuilderTest.java
@@ -1,12 +1,20 @@
 package parsing;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static simulation.model.Orientation.SOUTH;
+
+import java.util.LinkedList;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import lexing.model.AdventurerLineToken;
 import lexing.model.MapLineToken;
+import lexing.model.MountainLineToken;
+import lexing.model.TreasureLineToken;
 import parsing.exceptions.IllegalMapSizeException;
+import parsing.exceptions.OutboundTerrainException;
+import simulation.model.AdventurerAction;
 
 public class SimulationBuilderTest {
 	
@@ -41,6 +49,64 @@ public class SimulationBuilderTest {
 			assertThrows(IllegalMapSizeException.class, () -> new SimulationBuilder(mapLineToken));
 		}
 		
+	}
+	
+	@Nested
+	class TerrainInsideMap {
+		
+		@Test
+		void a_mountain_with_strictly_negative_position_should_result_in_OutboundTerrainException_first_coordinate() throws Exception {
+			MapLineToken mapLineToken = new MapLineToken(10, 15);
+			
+			SimulationBuilder builder = new SimulationBuilder(mapLineToken);
+			
+			assertThrows(OutboundTerrainException.class, () -> builder.withLine(new MountainLineToken(-1, 3)));
+		}
+		
+		@Test
+		void a_mountain_with_strictly_negative_position_should_result_in_OutboundTerrainException_second_coordinate() throws Exception {
+			MapLineToken mapLineToken = new MapLineToken(10, 15);
+			
+			SimulationBuilder builder = new SimulationBuilder(mapLineToken);
+			
+			assertThrows(OutboundTerrainException.class, () -> builder.withLine(new MountainLineToken(9, -3)));
+		}
+		
+		@Test
+		void a_treasure_with_strictly_negative_position_should_result_in_OutboundTerrainException_first_coordinate() throws Exception {
+			MapLineToken mapLineToken = new MapLineToken(10, 15);
+			
+			SimulationBuilder builder = new SimulationBuilder(mapLineToken);
+			
+			assertThrows(OutboundTerrainException.class, () -> builder.withLine(new TreasureLineToken(-9, 3, 1)));
+		}
+		
+		@Test
+		void a_treasure_with_strictly_negative_position_should_result_in_OutboundTerrainException_second_coordinate() throws Exception {
+			MapLineToken mapLineToken = new MapLineToken(10, 15);
+			
+			SimulationBuilder builder = new SimulationBuilder(mapLineToken);
+			
+			assertThrows(OutboundTerrainException.class, () -> builder.withLine(new TreasureLineToken(0, -1, 1)));
+		}
+		
+		@Test
+		void a_adventurer_with_strictly_negative_position_should_result_in_OutboundTerrainException_first_coordinate() throws Exception {
+			MapLineToken mapLineToken = new MapLineToken(10, 15);
+			
+			SimulationBuilder builder = new SimulationBuilder(mapLineToken);
+			
+			assertThrows(OutboundTerrainException.class, () -> builder.withLine(new AdventurerLineToken("adventurer's name", -1, 1, SOUTH, new LinkedList<AdventurerAction>())));
+		}
+		
+		@Test
+		void a_adventurer_with_strictly_negative_position_should_result_in_OutboundTerrainException_second_coordinate() throws Exception {
+			MapLineToken mapLineToken = new MapLineToken(10, 15);
+			
+			SimulationBuilder builder = new SimulationBuilder(mapLineToken);
+			
+			assertThrows(OutboundTerrainException.class, () -> builder.withLine(new AdventurerLineToken("adventurer's name", -1, 1, SOUTH, new LinkedList<AdventurerAction>())));
+		}
 	}
 	
 
