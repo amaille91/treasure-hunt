d7488138974beff8707835d595a7b1c495e4e3e1
Alexandre Maille <an email>
2022-02-13 11:15:24 +0100

    Implementation of the map size check

--- src/main/java/parsing/SimulationBuilder.java
+++ src/main/java/parsing/SimulationBuilder.java
@@ -2,11 +2,21 @@ package parsing;
 
 import lexing.model.LineToken;
 import lexing.model.MapLineToken;
+import parsing.exceptions.IllegalMapSizeException;
 
 public class SimulationBuilder {
 
+	private int horizontalNbOfBoxes;
+	private int verticalNbOfBoxes;
+
 	public SimulationBuilder(MapLineToken mapLineToken) {
-		throw new UnsupportedOperationException("not yet implemented");
+		int horizontalNbOfBoxes = mapLineToken.getHorizontalNbOfBoxes();
+		int verticalNbOfBoxes = mapLineToken.getVerticalNbOfBoxes();
+		if(horizontalNbOfBoxes < 1 || verticalNbOfBoxes < 1) {
+			throw new IllegalMapSizeException();
+		}
+		this.horizontalNbOfBoxes = horizontalNbOfBoxes;
+		this.verticalNbOfBoxes = verticalNbOfBoxes;
 	}
 	
 	public void withLine(LineToken token) {
