412abf35821583ff7d55f691fb1c4b0780fd4b5b
Alexandre Maille <an email>
2022-02-13 10:40:02 +0100

    TDD - an input file should have only one map line

--- src/test/java/parsing/ParserTest.java
+++ src/test/java/parsing/ParserTest.java
@@ -12,9 +12,11 @@ import org.junit.jupiter.api.Test;
 
 import lexing.model.AdventurerLineToken;
 import lexing.model.LineToken;
+import lexing.model.MapLineToken;
 import lexing.model.MountainLineToken;
 import lexing.model.TreasureLineToken;
 import parsing.exceptions.NoMapException;
+import parsing.exceptions.TooManyMapsException;
 
 public class ParserTest {
 	
@@ -28,6 +30,20 @@ public class ParserTest {
 				new AdventurerLineToken("Adventurer2", 1, 0, EAST, new LinkedList<>())));
 		
 		assertThrows(NoMapException.class, () -> Parser.parse(tokens));
+	}
+	
+	@Test
+	void parsing_tokens_with_multiple_map_tokens_should_result_in_TooManyMapsException() throws Exception {
+		Queue<LineToken> tokens = new LinkedList<>(Arrays.asList(
+				new MountainLineToken(1, 1),
+				new MountainLineToken(2, 3),
+				new MapLineToken(4, 5),
+				new TreasureLineToken(1, 2, 2),
+				new AdventurerLineToken("Adventurer1", 0, 1, SOUTH, new LinkedList<>()),
+				new MapLineToken(10, 10),
+				new AdventurerLineToken("Adventurer2", 1, 0, EAST, new LinkedList<>())));
+		
+		assertThrows(TooManyMapsException.class, () -> Parser.parse(tokens));
 	}
 
 }
