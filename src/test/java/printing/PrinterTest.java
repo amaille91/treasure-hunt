66caa1cbac5c392c78817ca770172d80120d8522
Alexandre Maille <an email>
2022-02-13 16:02:06 +0100

    TDD - Treasures should appear after mountains

--- src/test/java/printing/PrinterTest.java
+++ src/test/java/printing/PrinterTest.java
@@ -32,4 +32,16 @@ public class PrinterTest {
 		
 		assertThat(lines).startsWith("C - 3 - 4", "M - 0 - 0", "M - 2 - 3");
 	}
+	
+	@Test
+	void treasures_should_be_printed_after_mountains_in_order_of_their_position() throws Exception {
+		List<String> lines = Printer.toStrings(3, 4,
+				Map.of( new Position(2, 3), new Mountain(),
+						new Position(2, 2), new Adventurer("Adventurer", Orientation.EAST, List.of(), 0, 0),
+						new Position(1, 2), new Treasure(2),
+						new Position(0, 0), new Mountain(),
+						new Position(0, 2), new Treasure(1)));
+		
+		assertThat(lines).startsWith("C - 3 - 4", "M - 0 - 0", "M - 2 - 3", "T - 0 - 2 - 1", "T - 1 - 2 - 2");
+	}
 }
